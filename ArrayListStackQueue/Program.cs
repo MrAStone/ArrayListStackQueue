a263e75256163e76bc8fcbaf74a3510d9aff32f4
astone <astone@B103-SB>
2024-11-04 09:59:32 +0000

    Add project files.

---
+++ ArrayListStackQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ArrayListStackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            Console.WriteLine(numbers.Pop());
+            Console.WriteLine(numbers.Peek());
+            numbers.Pop();
+            Console.WriteLine(numbers.Peek());
+        }
+    }
+}
